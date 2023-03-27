1436b93512dee67ccc2cfff5110941274109d435
Ahsan Al Bashar <ahsan.cse.101792@gmail.com>
2023-03-27 16:15:33 +0600

    page and permission seeder

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Page;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PageSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class PageSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $title = 'About Us';
+        Page::updateOrCreate([
+            'page_title' => $title,
+            'page_slug' => Str::slug($title),
+            'page_short' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            'page_long' => "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum comes from a line in section 1.10.32.
+            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from  by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+            'meta_title' => 'about us know more about us',
+            'meta_keywords' => 'about us, know more, get in touch',
+            'meta_description' => 'This is a about page by letting you know about us',
+            'is_active' => true
+        ]);
     }
 }
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Module;
+use App\Models\Permission;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PermissionSeeder extends Seeder
 {
@@ -14,6 +17,60 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $adminPermissionArray = [
+            'Access Dashboard',
+        ];
+        $adminRolePermissionArray = [
+            'Index Role',
+            'Create Role',
+            'Edit Role',
+            'Delete Role',
+        ];
+        $adminUserPermissionArray = [
+            'Index User',
+            'Create User',
+            'Edit User',
+            'Delete User',
+        ];
+
+
+        // Access Dashboard
+        $adminDashboardModule = Module::where('module_name', 'Admin Dashboard')
+        ->select('id')
+        ->first();
+
+        Permission::updateOrCreate([
+            'module_id' => $adminDashboardModule->id,
+            'permission_name' => $adminPermissionArray[0],
+            'permission_slug' => Str::slug($adminPermissionArray[0])
+        ]);
+
+
+        // Role Management
+        $roleManagementModule = Module::where('module_name', 'Role Management')
+        ->select('id')
+        ->first();
+
+        for ($i=0; $i <count($adminRolePermissionArray); $i++) {
+            Permission::updateOrCreate([
+                'module_id' => $roleManagementModule->id,
+                'permission_name' => $adminRolePermissionArray[$i],
+                'permission_slug' => Str::slug($adminRolePermissionArray[$i])
+            ]);
+        }
+
+        // User Management
+        $userManagementModule = Module::where('module_name', 'User Management')
+        ->select('id')
+        ->first();
+
+        for ($i=0; $i <count($adminUserPermissionArray); $i++) {
+            Permission::updateOrCreate([
+                'module_id' => $userManagementModule->id,
+                'permission_name' => $adminUserPermissionArray[$i],
+                'permission_slug' => Str::slug($adminUserPermissionArray[$i])
+            ]);
+        }
+
     }
 }
