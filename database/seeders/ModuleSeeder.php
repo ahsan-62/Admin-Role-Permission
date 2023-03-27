eb75fc92baecbd804a875a9fb6dde421b9880432
Ahsan Al Bashar <ahsan.cse.101792@gmail.com>
2023-03-27 16:14:45 +0600

    module and permission seeder class

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Module;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ModuleSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class ModuleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $moduleArray = [
+            'Admin Dashboard',
+            'Role Management',
+            'User Management',
+            'Permission Management',
+        ];
+
+        foreach ($moduleArray as $module) {
+            Module::updateOrCreate([
+                'module_name' => $module,
+                'module_slug' => Str::slug($module),
+            ]);
+        }
     }
 }
