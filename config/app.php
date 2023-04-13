6904e7e16906813ec574819f92b4ba643264c87c
Ahsan Al Bashar <ahsan.cse.101792@gmail.com>
2023-04-14 01:14:43 +0600

    Setting Base With Helper Function

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        browner12\helpers\HelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
